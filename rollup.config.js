a5b006a987d4a8ea7470a45b93557b6e97212272
Ben Foxall <benfoxall@gmail.com>
2017-09-11 12:17:23 +0100

    Switch to being a generic ring buffer

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,10 @@ import buble from 'rollup-plugin-buble'
 export default {
   input: 'main.js',
   output: {
-    file: 'build/SRB.js',
-    format: 'iife'
+    file: 'build/RingBuffer.js',
+    format: 'umd'
   },
-  name: 'SRB',
+  name: 'RingBuffer',
   plugins: [
     buble()
   ]

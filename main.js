ccbf9d339071630bd9ebf5ca14fa97c477ac73cd
Ben Foxall <benfoxall@gmail.com>
2017-09-11 12:17:56 +0100

    Initial working implementation

--- main.js
+++ main.js
@@ -1,28 +1,41 @@
-class SRB {
-  constructor(n) {
+class RingBuffer {
+  constructor(ArrayType, n) {
+    this.ArrayType = ArrayType
 
-    if(n instanceof SharedArrayBuffer) {
-      this.buffer = n
+    // set up buffer, array & offset
+    if(Number.isFinite(n)) {
+      const bytes = (this.ArrayType.BYTES_PER_ELEMENT * n) + 2
+      this.buffer = new Uint8Array(bytes).buffer
     } else {
-      this.buffer = new SharedArrayBuffer(
-        (n+1) * Uint16Array.BYTES_PER_ELEMENT
-      )
+      throw new Error("Couldn't construct RingBuffer")
     }
 
-    this.size = (
-      this.buffer.byteLength / Uint16Array.BYTES_PER_ELEMENT
-    ) - 1
-
-    this.store = []
+    this.offset = new Uint16Array(this.buffer, 0, 2)
+    this.array = new ArrayType(this.buffer, 2)
   }
 
-  add(items) {
-    this.store = this.store.concat(items)
+  add(source) {
+    if(Array.isArray(source)) {
+      // todo - this could be optimised a lot with this.array.set
+      return source.forEach(v => this.add(v))
+    }
+
+    const offset = this.offset[0]
+    this.offset[0] = (this.offset[0] + 1) % this.array.length
+
+    this.array[offset] = source
   }
 
-  toArray() {
-    return this.store.slice(-this.size)
+  fill(target) {
+    const offset = this.offset[0]
+    const start = offset - target.length
+
+    // again, can be more optimised with target.set
+    for (var i = 0; i < target.length; i++) {
+      const idx = ((i + start) + this.array.length) % this.array.length
+      target[i] = this.array[idx]
+    }
   }
 }
 
-export default SRB
+export default RingBuffer
--- test/tests.js
+++ test/tests.js
@@ -1,62 +1,86 @@
 const expect = chai.expect
 
-describe('basic api', () => {
+describe('buffer store', () => {
 
-  it('stores', () => {
-    const ring = new SRB(10)
+  const ring = new RingBuffer(Uint16Array, 5)
+  const _store = new Uint16Array(ring.buffer)
 
+  it('sets initial', () => {
     ring.add([1,2,3])
-    ring.add([4,5,6])
+    expect(Array.from(_store))
+      .to.eql([3, 1, 2, 3, 0, 0])
+  })
 
-    expect(ring.toArray())
-      .to.eql([
-        1,2,3,4,5,6
-      ])
+  it('can be added to', () => {
+    ring.add([4])
+
+    expect(Array.from(_store))
+      .to.eql([4, 1, 2, 3, 4, 0])
+  })
+
+  it('goes back to start', () => {
+
+    ring.add([5])
+
+    expect(Array.from(_store))
+      .to.eql([0, 1, 2, 3, 4, 5])
 
   })
 
-  it('wraps items', () => {
-    const ring = new SRB(5)
+  it('wraps longer input', () => {
 
-    ring.add([1,2,3])
-    ring.add([4,5,6])
+    ring.add([6,7,8,9,10,11])
 
-    expect(ring.toArray())
-      .to.eql([
-        2,3,4,5,6
-      ])
+    expect(Array.from(_store))
+      .to.eql([1, 11, 7, 8, 9, 10])
+
+  })
+
+
+  it('wraps much longer input', () => {
+
+    ring.add([7,8,9,10,11,12,13,14,15,16,17])
+
+    expect(Array.from(_store))
+      .to.eql([2, 16, 17, 13, 14, 15])
 
   })
 
 })
 
+describe('output', () => {
 
-describe('shared buffer', () => {
+  const ring = new RingBuffer(Uint16Array, 10)
+  ring.add([1,2,3,4,5,6])
 
-  const ring = new SRB(5)
-  const ring2 = new SRB(ring.buffer)
+  it('takes last elements', () => {
+    const target = new Uint16Array(5)
 
-  ring.add([1,2,3])
+    ring.fill(target)
 
-  it('has a shared backing buffer', () => {
-    expect(ring.buffer)
-      .to.be.a('SharedArrayBuffer')
+    expect(Array.from(target))
+      .to.eql([2,3,4,5,6])
 
-    expect(ring.buffer)
-      .to.equal(ring2.buffer)
+    const target_sm = new Uint16Array(3)
+
+    ring.fill(target_sm)
+
+    expect(Array.from(target_sm))
+      .to.eql([4,5,6])
+
   })
 
-  it('initialises the second ring', () => {
-    expect(ring2.size)
-      .to.eql(5)
+  const s_ring = new RingBuffer(Uint16Array, 4)
+  s_ring.add([1,2,3,4,5,6,7])
+
+  it('wraps around', () => {
+    const target = new Uint16Array(4)
+
+    s_ring.fill(target)
+
+    expect(Array.from(target))
+      .to.eql([4,5,6,7])
+
   })
-
-  xit('populates across', () => {
-    expect(ring2.toArray())
-      .to.eql([
-        1,2,3
-      ])
-  })
-
-
+  
 })
